5f2e08f39b0a50b109aadd44d112a28f40997167
Aly Sivji <alysivji@gmail.com>
2017-05-13 10:36:27 -0500

    Refactored to use pytest fixture

--- tests/deck_test.py
+++ tests/deck_test.py
@@ -1,5 +1,10 @@
 from carddeck.deck import Card, Deck
+import pytest
 
+
+###################
+## Card Class Tests
+###################
 
 def test_create_card():
     new_card = Card(suit='hearts', rank=8)
@@ -8,21 +13,27 @@ def test_create_card():
     assert new_card.rank == 8
 
 
-def test_create_deck():
-    deck = Deck()
+###################
+## Deck Class Tests
+###################
+
+@pytest.fixture
+def deck():
+    return Deck()
+
+
+def test_create_deck(deck):
     assert len(deck) == 52
 
 
-def test_deck_deal():
-    deck = Deck()
+def test_deck_deal(deck):
     card_dealt = deck.deal()
 
     assert card_dealt not in deck
     assert len(deck) == 51
 
 
-def test_shuffle():
-    deck = Deck()
+def test_shuffle(deck):
     unshuffled_deck = deck.copy()
     deck.shuffle()
 
