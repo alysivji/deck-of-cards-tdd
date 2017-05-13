e119234491b1594d2f45fc11a669d0ad5ed1eba2
Aly Sivji <alysivji@gmail.com>
2017-05-13 08:46:35 -0500

    added shuffle function and test

--- carddeck/deck.py
+++ carddeck/deck.py
@@ -1,4 +1,5 @@
 from collections import deque
+import random
 
 
 class Card:
@@ -9,10 +10,23 @@ class Card:
         self.suit = suit
         self.rank = rank
 
+    def __len__(self):
+        return 1
+
+    def __repr__(self):
+        return f'{self.rank} of {self.suit}'
+
+    def __eq__(self, other):
+        if self.suit == other.suit:
+            if self.rank == other.rank:
+                return True
+
+        return False
+
 
 class Deck(deque):
 
-    def __init__(self):
+    def __init__(self, *args, **kwargs):
         '''Create standard deck of card'''
 
         RANKS = [2, 3, 4, 5, 6, 7, 8, 9, 10, 'J', 'Q', 'K', 'A']
@@ -21,3 +35,11 @@ class Deck(deque):
         for suit in SUITS:
             for rank in RANKS:
                 self.append(Card(suit=suit, rank=rank))
+
+    def deal(self):
+        '''Deal single card'''
+        return self.popleft()
+
+    def shuffle(self):
+        '''Shuffle deck in place'''
+        random.shuffle(self)
--- tests/deck_test.py
+++ tests/deck_test.py
@@ -11,3 +11,19 @@ def test_create_card():
 def test_create_deck():
     deck = Deck()
     assert len(deck) == 52
+
+
+def test_deck_deal():
+    deck = Deck()
+    card_dealt = deck.deal()
+
+    assert card_dealt not in deck
+    assert len(deck) == 51
+
+
+def test_shuffle():
+    deck = Deck()
+    unshuffled_deck = deck.copy()
+    deck.shuffle()
+
+    assert deck != unshuffled_deck
