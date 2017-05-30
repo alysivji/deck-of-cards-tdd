4b0020768158d3ec16cfee218a4f4605be1e4b78
Aly Sivji <alysivji@gmail.com>
2017-05-30 18:44:23 -0500

    Stop flake8 from complanining

--- tests/deck_test.py
+++ tests/deck_test.py
@@ -2,9 +2,9 @@ from carddeck.deck import Card, Deck, Hand
 import pytest
 
 
-###################
-## Card Class Tests
-###################
+##################
+# Card Class Tests
+##################
 
 def test_create_card():
     new_card = Card(suit='hearts', rank=8)
@@ -13,9 +13,9 @@ def test_create_card():
     assert new_card.rank == 8
 
 
-###################
-## Deck Class Tests
-###################
+##################
+# Deck Class Tests
+##################
 
 @pytest.fixture
 def deck():
@@ -47,17 +47,17 @@ def test_deal_hand(deck):
 
     # dealing more cards than possible
     with pytest.raises(ValueError):
-        hands =  deck.deal_hands(num_players=53, num_cards=1)
+        hands = deck.deal_hands(num_players=53, num_cards=1)
 
 
-###################
-## Hand Class Tests
-###################
+##################
+# Hand Class Tests
+##################
 
 @pytest.fixture
 def make_test_hand():
     hand = Hand()
-    
+
     card1 = Card(suit='hearts', rank=3)
     card2 = Card(suit='spades', rank=8)
     card3 = Card(suit='spades', rank='A')
@@ -67,13 +67,16 @@ def make_test_hand():
 
     return hand
 
+
 def test_create_empty_hand():
     hand = Hand()
     assert len(hand) == 0
 
+
 def test_create_hand_with_multiple_cards(make_test_hand):
     hand = make_test_hand
     assert len(hand) == 3
+
 
 def test_score_hand(make_test_hand):
     '''Get score... default implementation gets max card (Aces high)'''
